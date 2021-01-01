e4f80058d1cf5abe4aca04904a652652e6c63249
rkckr1 <rkckr1@naver.com>
2021-01-02 01:27:28 +0900

    1.02

--- ch08/palindrome_linked_list.py
+++ ch08/palindrome_linked_list.py
@@ -41,5 +41,15 @@ class Solution:
         return True
 
     def isPalindrome_C(self, head):
-        
+        rev = None
+        slow = fast = head
 
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+        if fast:
+            slow = slow.next
+
+        while rev and rev.val == slow.val:
+            slow, rev = slow.next, rev.next
+        return not rev
