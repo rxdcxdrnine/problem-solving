dc2d4bc6d7bca4af59560156a71400b0ff78fe4a
rkckr1 <rkckr1@naver.com>
2021-02-22 01:27:45 +0900

    2.22

---
+++ ch10/77.combinations.py
@@ -0,0 +1,55 @@
+# leetcode 77
+
+import itertools
+
+def combine_0(n, k):
+    nums = list(range(1, n + 1))
+
+    graph = {}
+    for i, num in enumerate(nums):
+        graph[num] = nums[i + 1:]
+
+    result = []
+    def recursive_dfs(v, depth, discovered):
+        discovered.append(v)
+        depth += 1
+
+        if depth == k:
+            result.append(discovered[:])
+        else:
+            for w in graph[v]:
+                if w not in discovered:
+                    recursive_dfs(w, depth, discovered)
+                    discovered.pop()
+
+        depth -= 1
+        return discovered
+
+    for num in nums:
+        recursive_dfs(num, 0, [])
+
+    return result
+
+
+def combine_A(n, k):
+    results= []
+
+    def dfs(elements, start, k):
+        if k == 0:
+            results.append(elements[:])
+
+        for i in range(start, n + 1):
+            elements.append(i)
+            dfs(elements, i + 1, k - 1)
+            elements.pop()
+
+    dfs([], 1, k)
+    return results
+
+
+def combine_B(n, k):
+    return list(itertools.combinations(range(1, n + 1), k))
+
+
+if __name__ == "__main__":
+    print(combine_0(4, 2))
