cd47e638119912419581f0d659691b6dd318762a
Kang Changgu (Miller) <rkckr1@naver.com>
2022-05-18 16:40:22 +0900

    BOJ 1149 추가

--- python/boj/BOJ_1149.py
+++ python/boj/BOJ_1149.py
@@ -1,6 +1,33 @@
-def solution() -> None:
-    pass
+import sys
+from typing import List
+
+
+def solution(N: int, costs: List[List[int]]) -> int:
+    dp: List[List[int]] = [[0 for y in range(3)] for x in range(N)]
+
+    for ind in range(3):
+        dp[0][ind] = costs[0][ind]
+
+    for i in range(1, N):
+        for j in range(3):
+            min_cost: int = sys.maxsize
+            for k in range(3):
+                if k == j:
+                    continue
+
+                if min_cost > dp[i - 1][k]:
+                    min_cost = dp[i - 1][k]
+
+            dp[i][j] = costs[i][j] + min_cost
+
+    return min(dp[-1])
 
 
 if __name__ == "__main__":
-    solution()
+    N: int = int(input())
+    costs: List[List[int]] = []
+
+    for _ in range(N):
+        costs.append(list(map(int, input().split())))
+
+    print(solution(N, costs))
